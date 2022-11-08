bc75f860d7785408bd55e24dc764207b09738d44
Woody1193 <ryanwood1193@gmail.com>
2022-11-08 01:41:00 +0000

    Replaced bytes with data.Decimal in trade request

--- data/common.pb.go
+++ data/common.pb.go
@@ -85,6 +85,55 @@ func (x *NameResult) GetAddress() string {
 	return ""
 }
 
+// Decimal is an internal type that allows for transferring information to and from an exact Decimal
+// value, so that it can be sent over the wire between processes
+type Decimal struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Data []byte `protobuf:"bytes,1,opt,name=data,proto3" json:"data,omitempty"`
+}
+
+func (x *Decimal) Reset() {
+	*x = Decimal{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_edge_data_common_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Decimal) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Decimal) ProtoMessage() {}
+
+func (x *Decimal) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_edge_data_common_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Decimal.ProtoReflect.Descriptor instead.
+func (*Decimal) Descriptor() ([]byte, []int) {
+	return file_protos_edge_data_common_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *Decimal) GetData() []byte {
+	if x != nil {
+		return x.Data
+	}
+	return nil
+}
+
 var File_protos_edge_data_common_proto protoreflect.FileDescriptor
 
 var file_protos_edge_data_common_proto_rawDesc = []byte{
@@ -95,10 +144,12 @@ var file_protos_edge_data_common_proto_rawDesc = []byte{
 	0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12,
 	0x12, 0x0a, 0x04, 0x74, 0x79, 0x70, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x74,
 	0x79, 0x70, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x18, 0x03,
-	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x42, 0x27, 0x5a,
-	0x25, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x78, 0x65, 0x66, 0x69,
-	0x6e, 0x6f, 0x2f, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d, 0x2d, 0x63, 0x6c, 0x69, 0x2d, 0x67,
-	0x6f, 0x2f, 0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x22, 0x1d, 0x0a,
+	0x07, 0x44, 0x65, 0x63, 0x69, 0x6d, 0x61, 0x6c, 0x12, 0x12, 0x0a, 0x04, 0x64, 0x61, 0x74, 0x61,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x04, 0x64, 0x61, 0x74, 0x61, 0x42, 0x27, 0x5a, 0x25,
+	0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x78, 0x65, 0x66, 0x69, 0x6e,
+	0x6f, 0x2f, 0x71, 0x75, 0x61, 0x6e, 0x74, 0x75, 0x6d, 0x2d, 0x63, 0x6c, 0x69, 0x2d, 0x67, 0x6f,
+	0x2f, 0x64, 0x61, 0x74, 0x61, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -113,9 +164,10 @@ func file_protos_edge_data_common_proto_rawDescGZIP() []byte {
 	return file_protos_edge_data_common_proto_rawDescData
 }
 
-var file_protos_edge_data_common_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protos_edge_data_common_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
 var file_protos_edge_data_common_proto_goTypes = []interface{}{
 	(*NameResult)(nil), // 0: protos.edge.data.NameResult
+	(*Decimal)(nil),    // 1: protos.edge.data.Decimal
 }
 var file_protos_edge_data_common_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -143,6 +195,18 @@ func file_protos_edge_data_common_proto_init() {
 				return nil
 			}
 		}
+		file_protos_edge_data_common_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Decimal); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -150,7 +214,7 @@ func file_protos_edge_data_common_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_edge_data_common_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   1,
+			NumMessages:   2,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
