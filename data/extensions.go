41c48af03e39fb19b349e896b6dd0032587acb24
Woody1193 <ryanwood1193@gmail.com>
2022-11-14 16:46:31 +0900

    Integrated refactored Decimal value into extensions

--- data/extensions.go
+++ data/extensions.go
@@ -1,41 +1,74 @@
 package data
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
+	"math"
+	"strconv"
 
 	"github.com/shopspring/decimal"
 )
 
 // NewFromDecimal creates a new representation of our Decimal from a decimal.Decimal
-func NewFromDecimal(in decimal.Decimal) (*Decimal, error) {
+func NewFromDecimal(in decimal.Decimal) *Decimal {
+	const width = 18
 
-	// Create our encoder from a new bytes buffer
-	buf := new(bytes.Buffer)
-	encoder := gob.NewEncoder(buf)
+	// First, convert the input decimal coefficient to a big.Int and from there to a string
+	asStr := in.Coefficient().Text(10)
+	strLen := len(asStr)
 
-	// Attempt to gob-encode the data to a Decimal; if this fails then return an error
-	if err := encoder.Encode(in); err != nil {
-		return nil, fmt.Errorf("failed to encode Decimal, error: %v", err)
+	// Next, if the original value was negative then we'll calculate an offset to use when
+	// calculating the number of int64's and when propogating the negative sign
+	offset := 0
+	if in.IsNegative() {
+		offset = 1
 	}
 
-	// The encoding succeeded; return the decimal
-	return &Decimal{Data: buf.Bytes()}, nil
+	// Now, determine the number of int64's we want to split the value into
+	length := int(math.Ceil(float64(strLen-offset) / width))
+
+	// Finally, iterate over each of the int64's and extract a portion of the string to load into it
+	ints := make([]int64, length)
+	for i := 1; i <= length; i++ {
+
+		// First, calculate the start position (we want this to be from the end of the string)
+		start := strLen - (i * width)
+
+		// Next, calculate the end of the string, which should have a constant value
+		end := start + width
+
+		// Now, if we're at risk of walking off the front of the string then set the start value to zero
+		if start < 0 || (start == 1 && asStr[0] == '-') {
+			start = 0
+		}
+
+		// Finally, parse the string back into an integer; since we came from an integer this cannot fail
+		// If the value we're parsing from is negative but we're working with an intermediate value then
+		// we'll need to be sure that the negative sign is propogated
+		ints[i-1], _ = strconv.ParseInt(asStr[start:end], 10, 64)
+		if offset == 1 && ints[i-1] > 0 {
+			ints[i-1] = 0 - ints[i-1]
+		}
+	}
+
+	// Inject the parts and the exponent into a Decimal value and return it
+	return &Decimal{Value: ints, Exp: in.Exponent()}
 }
 
 // ToDecimal converts our internal representation of a Decimal to a decimal.Decimal
-func (d *Decimal) ToDecimal() (*decimal.Decimal, error) {
-	var out decimal.Decimal
+func (d *Decimal) ToDecimal() *decimal.Decimal {
 
-	// Create our decoder from a buffer of the data
-	decoder := gob.NewDecoder(bytes.NewBuffer(d.Data))
+	// First, create our decimal value with a zero-value
+	resp := decimal.New(0, 0)
 
-	// Attempt to gob-decode the data to a Decimal; if this fails then return an error
-	if err := decoder.Decode(&out); err != nil {
-		return nil, fmt.Errorf("failed to decode to Decimal, error: %v", err)
+	// Next, iterate over all our sub-values and add them into the total
+	for i, value := range d.Value {
+
+		// Attempt to convert the value to its decimal equivalent based on where it is in the list
+		temp := decimal.New(value, int32(i*18)+d.Exp)
+
+		// Add the temporary value to the total
+		resp = resp.Add(temp)
 	}
 
-	// The decoding succeeded; return the decimal
-	return &out, nil
+	// Finally, return our total
+	return &resp
 }
--- data/extensions_test.go
+++ data/extensions_test.go
@@ -8,50 +8,52 @@ import (
 
 var _ = Describe("Decimal Extensions Tests", func() {
 
-	// Tests that, if the gob encoder does not fail, then calling NewFromDecimal will encode the decimal
-	// into a GOB and write it to the data in the Decimal object which will then be returned
-	It("NewFromDecimal - No failures - Encoded", func() {
+	// Tests that, if the value is greater than 0, then it will be encoded properly
+	It("NewFromDecimal - Value greater than 0 - Encoded", func() {
 
 		// First, create our decimal value
-		dIn, err := decimal.NewFromString("52.99")
+		dIn, err := decimal.NewFromString("1234512351234088800000.999")
 		Expect(err).ShouldNot(HaveOccurred())
 
-		// Next, attempt to convert it to a new Decimal object; this should not fail
-		dOut, err := NewFromDecimal(dIn)
-		Expect(err).ShouldNot(HaveOccurred())
+		// Next, attempt to convert it to a new Decimal object
+		dOut := NewFromDecimal(dIn)
 
 		// Finally, verify the data
 		Expect(dOut).ShouldNot(BeNil())
-		Expect(string(dOut.Data)).Should(Equal("\x13\xff\x81\x05\x01\x01\aDecimal\x01\xff\x82\x00\x00\x00\v\xff\x82\x00\a\xff\xff\xff\xfe\x02\x14\xb3"))
+		Expect(dOut.Exp).Should(Equal(int32(-3)))
+		Expect(dOut.Value).Should(HaveLen(2))
+		Expect(dOut.Value[0]).Should(Equal(int64(351234088800000999)))
+		Expect(dOut.Value[1]).Should(Equal(int64(1234512)))
 	})
 
-	// Tests that, if the gob decoder fails, then calling ToDecimal will return an error
-	It("ToDecimal - Decode fails - Error", func() {
+	// Test that, if the value is less than 0, then it will be encoded properly
+	It("NewFromDecimal - Value less than 0 - Encoded", func() {
 
-		// First, create an invalid Decimal value
-		dIn := Decimal{Data: []byte("derp")}
-
-		// Next, attempt to convert the Decimal to a decimal; this should fail
-		dOut, err := dIn.ToDecimal()
-
-		// Finally, verify the error
-		Expect(dOut).Should(BeNil())
-		Expect(err).Should(HaveOccurred())
-		Expect(err.Error()).Should(Equal("failed to decode to Decimal, error: unexpected EOF"))
-	})
-
-	// Tests that, if the gob decoder does not fail, then calling ToDecimal will not return an error,
-	// but will decode the Decimal to a decimal value
-	It("ToDecimal - No failures - Decoded", func() {
-
-		// First, create a valid Decimal value
-		dIn := Decimal{Data: []byte("\x13\xff\x81\x05\x01\x01\aDecimal\x01\xff\x82\x00\x00\x00\v\xff\x82\x00\a\xff\xff\xff\xfe\x02\x14\xb3")}
-
-		// Next, attempt to convert the Decimal to a decima; this should not fail
-		dOut, err := dIn.ToDecimal()
+		// First, create our decimal value
+		dIn, err := decimal.NewFromString("-288341660781234512351234088800000.999")
 		Expect(err).ShouldNot(HaveOccurred())
 
+		// Next, attempt to convert it to a new Decimal object
+		dOut := NewFromDecimal(dIn)
+
 		// Finally, verify the data
-		Expect(dOut.String()).Should(Equal("52.99"))
+		Expect(dOut).ShouldNot(BeNil())
+		Expect(dOut.Exp).Should(Equal(int32(-3)))
+		Expect(dOut.Value).Should(HaveLen(2))
+		Expect(dOut.Value[0]).Should(Equal(int64(-351234088800000999)))
+		Expect(dOut.Value[1]).Should(Equal(int64(-288341660781234512)))
+	})
+
+	// Tests that the Decimal value will be converted to a decimal.Decimal properly
+	It("ToDecimal - Decoded", func() {
+
+		// First, create a valid Decimal value
+		dIn := Decimal{Value: []int64{351234088800000999, 1234512}, Exp: -3}
+
+		// Next, attempt to convert the Decimal to a decimal
+		dOut := dIn.ToDecimal()
+
+		// Finally, verify the data
+		Expect(dOut.String()).Should(Equal("1234512351234088800000.999"))
 	})
 })
